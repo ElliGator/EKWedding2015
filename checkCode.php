47bde19699c1167e37ae11f8ec7a0ba6b1ea65ac
cwhitten1 <cwhitten@ufl.edu>
2015-03-18 12:46:10 -0400

    Fixed checkCode

--- checkCode.php
+++ checkCode.php
@@ -1,5 +1,8 @@
 #!/usr/local/bin/php
 <?
+	
+	$con_string = "host=postgres.cise.ufl.edu port=5432 dbname=ekweddb user=ec1 password=SQLpassword12";
+	
 	//connection settings go here 
 	$dbconn = pg_connect($con_string);
 
@@ -11,8 +14,11 @@
 	$usr_code = $_GET['code'];
 	$checkcode = "SELECT code FROM codes WHERE code LIKE '$usr_code%'";
 
-	if(!$checkcode)
-		return 'false';
-	else 
+	$code_valid = pg_num_rows(pg_query($checkcode));
+
+
+	if($code_valid > 0)
 		return 'true';
+	else 
+		return 'false';
 ?>
