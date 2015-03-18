f2ff8c42faffaa0e742ec1e572e6f0762198be9b
cwhitten1 <cwhitten@ufl.edu>
2015-03-18 10:41:51 -0400

    Fixed addGuests.php
    Still needs connection string

--- addGuests.php
+++ addGuests.php
@@ -1,10 +1,7 @@
 #!\usr\local\bin\php
 <?php
-	/*What I want: 
-		1. When form is submitted I want to check the code for to make sure it exists in the db
-		2. Insert the party leader and his/her guests into guestlist to keep count of how many people
-			are coming
-	*/
+	
+	$con_string = ""
 
 	//connection settings go here
 	$dbconn = pg_connect($con_string);
@@ -16,26 +13,31 @@
 
 	
 	//Receive form values, guests is an array
-	$gst_code = pg_escape_string($_GET['code']);
-	$gst_leader = pg_escape_string($_POST['main_guest']);
-	$guests = pg_escape_string($_POST['guests']);
+	$gst_code = $_GET['code'];
+	$gst_leader = $_POST['main_guest'];
 
 	//Insert guest leader into db
-	$sql_gst_leader = "INSERT INTO guestlist VALUES ('$gst_leader ', '$gst_code', 'T')";
-	$result1 = pg_query($sql_gst_leader);
+	$sql_gst_leader = "INSERT INTO guests VALUES ($gst_code, '$gst_leader', '$gst_leader')" ;
+	$leader_result = pg_query($sql_gst_leader);
+
+
+
+	if(isset($_POST['guests']))
+	{
+		$guests = $_POST['guests']);
+
+		//Iterate through guests array and insert into db
+		$sql_gst = "INSERT INTO guests VALUES";
+		foreach (($guests as $x)) {
+			$sql_gst .= "('$gst_code', '$gst_leader', '$x'),";
+		}
+		$sql_gst = rtrim($sql_gst,",");
+		$sql_gst .= ";";
+		$guests_result = pg_query($sql_gst);
+	}
+
+		
 
 	
-	//Iterate through guests array and insert into db
-	$sql_gst = "INSERT INTO guestlist VALUES";
-	foreach (($guests as $x) && (count($guests)-1)) {
-		$sql_gst .= "('$x', '$gst_code', 'F'),";
-	}
-	$sql_gst = rtrim($sql_gst,",");
-	$sql_gst .= ";";
-	$result2 = pg_query($sql_gst);
 	
-	//Query total amount of people who have RSVPed
-	$rsvp_tot = pgsql_query($con, "SELECT count(*) FROM guestlist");
-	echo "$rsvp_tot";
-	pg_close($dbconn);
 ?>
